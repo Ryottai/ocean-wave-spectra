412154b67b07bb7ad08c36c63a91250e7918eb43
Ryottai <rmkr.t.2335.625@gmail.com>
2022-11-28 00:22:30 +0900

    fix app.py

--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import numpy as np
 import plotly.graph_objects as go
 from scipy.special import erfc
 
-omega = np.linspace(0,2,200)
+omega = np.linspace(0,20,4000)
 g = 9.81
 
 def derivative(f, omega, eps=0.000001):
@@ -29,7 +29,7 @@ def plot_function(func, title, Hv=None, Tv=None, Hs=None):
     else:
         fig.add_trace(go.Scatter(x=omega, y=func(omega), mode='lines', line=dict(color='navy', width=2)))
     fig.update_layout(title = title, xaxis_title='freakency',width=700, height=400, font=dict(family="Courier New, monospace",size=18,color="black"), margin=dict(t=30, b=0, l=0, r=0))
-    fig.update_xaxes(zeroline=True, zerolinewidth=2, zerolinecolor='gray', gridcolor='silver')
+    fig.update_xaxes(zeroline=True, zerolinewidth=2, zerolinecolor='gray', gridcolor='silver', range=(0,2))
     fig.update_yaxes(zeroline=True, zerolinewidth=2, zerolinecolor='gray', gridcolor='silver')
 
     return fig
